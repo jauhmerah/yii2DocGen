0807e6dc89b643cb419589638e1d9fa3f4ca30b3
FARID-CODETITAN\Codetitan <jauhmerah@gmail.com>
2018-08-25 20:37:54 +0800

    setup rbac

--- common/config/main.php
+++ common/config/main.php
@@ -10,4 +10,20 @@ return [
             'class' => 'yii\caching\FileCache',
         ],
     ],
+    'modules' => [
+        'admin' => [
+            'class' => 'mdm\admin\module',
+        ],
+    ],
+    'components' => [
+        'authManager' => [
+            'class' => 'yii\rbac\PhpManager',
+        ],
+        'urlManager' => [
+            'class' => 'yii\web\UrlManager',
+            'showScriptName' => false,
+            'enablePrettyUrl' => true,
+            'rules' => [],
+        ]
+    ],
 ];
